da2df2931abd071dd43395b0947b3688c29e546c
code0110 <cod.binar@gmail.com>
2020-08-08 09:09:44 +0300

    initial release

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -65,7 +65,7 @@
        <a href="{{ route('social.oauth', 'google') }}" class="btn btn-danger btn-block">
             Login with Google
        </a>
-       <a href="{{ route('social.oauth', 'github') }}" class="btn btn-default btn-block">
+       <a href="{{ url('auth/github') }}" class="btn btn-default btn-block">
               Login with Github
    </a> 
 
